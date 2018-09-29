b3fa0de1b8cc3298435ce8a973693d0842251310
mizukmb <mizukmb6@gmail.com>
2018-09-29 17:54:36 +0900

    esa-nippou v0.6.1

--- esa-nippou.rb
+++ esa-nippou.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-ESA_NIPPOU_VERSION = 'v0.6.0'
+ESA_NIPPOU_VERSION = 'v0.6.1'
 
 class EsaNippou < Formula
   homepage 'https://github.com/mizukmb/esa-nippou'
   version ESA_NIPPOU_VERSION
 
   url "https://github.com/mizukmb/esa-nippou/releases/download/#{ESA_NIPPOU_VERSION}/esa-nippou"
-  sha256 '6751f5f84b30bc409d56b62af846854dc7870b210533a1ed7718c209d1f2b8f4'
+  sha256 'cf5e2546fcc90efe621f149d7b3a746147bfd1776ff4ff20e4337c2ad5c0ca33'
 
   def install
     bin.install 'esa-nippou'
