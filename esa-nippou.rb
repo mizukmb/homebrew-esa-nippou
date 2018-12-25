42bd031c0c8c673cba1a88ac869519c6005d8407
Yuto MIZUSHIRI <mizukmb6@gmail.com>
2018-12-25 18:53:15 +0900

    Update esa-nippou.rb

--- esa-nippou.rb
+++ esa-nippou.rb
@@ -6,7 +6,7 @@ class EsaNippou < Formula
   homepage 'https://github.com/mizukmb/esa-nippou'
   version ESA_NIPPOU_VERSION
 
-  url "https://github.com/mizukmb/esa-nippou/releases/download/#{ESA_NIPPOU_VERSION}/esa-nippou"
+  url "https://github.com/mizukmb/esa-nippou/releases/download/#{ESA_NIPPOU_VERSION}/esa-nippou_#{ESA_NIPPOU_VERSION}_x86_64-apple-darwin.zip"
   sha256 'b979549b287a4bec4962fa66cae1033251fa5f88f0362e1512ace9445fdf3a1c'
 
   def install
