3c0b97467d1d7e1f4eb238df5e39d6d2cb11b0bc
Yuto MIZUSHIRI <mizukmb6@gmail.com>
2019-11-01 00:33:14 +0900

    Bump v1.1.1

--- esa-nippou.rb
+++ esa-nippou.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-ESA_NIPPOU_VERSION = 'v1.1.0'
+ESA_NIPPOU_VERSION = 'v1.1.1'
 
 class EsaNippou < Formula
   homepage 'https://github.com/mizukmb/esa-nippou'
   version ESA_NIPPOU_VERSION
 
   url "https://github.com/mizukmb/esa-nippou/releases/download/#{ESA_NIPPOU_VERSION}/esa-nippou_#{ESA_NIPPOU_VERSION}_x86_64-apple-darwin.zip"
-  sha256 '73f51e5f3068a045b9c2368d8e52778b3bf5725d12b839aadfd65637d4628979'
+  sha256 '7c217737e315efe9ba7af3bb92779b10fa5de7e17cb2ac9f1ad1df81de79b9c8'
   
   def install
     bin.install 'esa-nippou'
